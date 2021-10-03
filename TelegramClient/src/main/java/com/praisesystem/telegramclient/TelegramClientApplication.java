9a9ec651f1d5450b09c149bab1b406ef31169d8a
hurek <rudrobinrood@gmail.com>
2021-10-04 00:21:15 +0300

    dev: Added modules for Telegram and Discord clients (bots)

---
+++ TelegramClient/src/main/java/com/praisesystem/telegramclient/TelegramClientApplication.java
@@ -0,0 +1,13 @@
+package com.praisesystem.telegramclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramClientApplication.class, args);
+    }
+
+}
